58637dbe8fae536be54d12e805c9fd140b83d5de
shan wolff <shanwolff182@gmail.com>
2023-10-28 13:31:28 -0400

    Finished

--- scripts.js
+++ scripts.js
@@ -1,50 +1,137 @@
-//variables
-let searchBtnVariable = document.querySelector("#search-button");
-//In your project, create a variable called userInput (or anything that makes sense to you), and save the input element into it
-let userInput = document.querySelector("#input");
-let userLocation = "London";
-let cityElement = document.querySelector("#city");
+let search = document.querySelector("#searchButton");
+let locationInput = document.querySelector("#inputLocationBox");
+let temperatureTypeDiv = document.querySelector("#temp-type");
+let headerText = document.querySelector("#three-day-forecast-header");
+let highTempDay = document.querySelector("#hottest-day-info");
 
-let todayObj = {
+let today = {
+    city: "",
+    state: "",
+    country: "",
     currentTemp: "",
-    currentCity: "",
-    currentState: "",
-    currentCountry: "",
-}
+    highTemp: "",
+    lowTemp: "",
+    chanceOfPrecipitation: ""
+};
 
 let threeDayForecast = [];
+let dayOfWeek = [];
+let fourDayForecastTemp = [];
+let highestTempDay = 0;
 
-searchBtnVariable.addEventListener("click", function() {
-    //in the first line of your function, create a variable called cityFromUser and set it equal to the value of userInput. 
-    let cityFromUser = userInput.value;
-    console.log(cityFromUser);
-    
-    fetch(`http://api.weatherapi.com/v1/forecast.json?key=ad3cf70874184ceb9b9182403232609&q=${cityFromUser}&days=4`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        todayObj.currentTemp = data.current.temp_f;
-        //1. loop over the array of three
-        //  2. for each item, create a object that has the keys you need (average temp, precip, etc)
-        //  3. push that object into threeDayForecast
-        for (let i = 1; i < data.forecast.forecastday.length; i++) {
-            let forecast = {
-                date: data.forecast.forecastday[i].date,
-                averageTemp: data.forecast.forecastday[i].avgtemp_f,
-                hightTemp: data.forecast.forecastday[i].maxtemp_f,
-                lowTemp: data.forecast.forecastday[i].mintemp_f,
-                precipitation: data.forecast.forecastday[i].daily_chance_of_rain,
-            };
-            threeDayForecast.push(forecast);
+search.addEventListener("click", function() {
+    threeDayForecast = [];
+    let userLocation = locationInput.value;
+    console.log("clicked")
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=a723218da53a464d8cf182437232609&q=${userLocation}&days=4`)
+        .then(response => response.json()) 
+        .then(data => {
+            today.city = data.location.name;
+            today.state = data.location.region;
+            today.country = data.location.country;
+            today.currentTemp = Math.round(data.current.temp_f);
+            today.highTemp = Math.round(data.forecast.forecastday[0].day.maxtemp_f);
+            today.lowTemp = Math.round(data.forecast.forecastday[0].day.mintemp_f);
+            today.chanceOfPrecipitation = data.forecast.forecastday[0].day.daily_chance_of_rain;
+            
+            for (let i = 1; i < data.forecast.forecastday.length; i++) {
+                let forecast = {
+                    date: data.forecast.forecastday[i].date,
+                    averageTemp: Math.round(data.forecast.forecastday[i].day.avgtemp_f),
+                    highTemp: Math.round(data.forecast.forecastday[i].day.maxtemp_f),
+                    lowTemp: Math.round(data.forecast.forecastday[i].day.mintemp_f),
+                    chanceOfPrecipitation: data.forecast.forecastday[i].day.daily_chance_of_rain,
+                    day: getDayOfWeek(data.forecast.forecastday[i].date)
+                };
+                threeDayForecast.push(forecast);
+
+            }
+            for (let i = 0; i <data.forecast.forecastday.length; i++) {
+                let dayTempObj = {
+                    dayOfWeek: getDayOfWeek(data.forecast.forecastday[i].date),
+                    temp:data.forecast.forecastday[i].day.maxtemp_f,
+                }
+                fourDayForecastTemp.push(dayTempObj);
+                }
+            for (let i = 0; i<fourDayForecastTemp.length; i++) {
+                if (fourDayForecastTemp[i].temp > highestTempDay) {
+                highestTempDay = fourDayForecastTemp[i].dayOfWeek;
+                }
+                }
+            updateUI();
+            
+        });
+        
+    });
+        function updateUI() {
+        document.querySelector("#todays-weather-header").textContent = "Today's Weather";
+        
+        document.querySelector("#city").textContent = today.city + ",";
+        document.querySelector("#state").textContent = today.state;
+        document.querySelector("#country").textContent = today.country;
+        document.querySelector("#current-temp").textContent = today.currentTemp + "°F";
+        document.querySelector("#high-temp").textContent = "Today's High: " + today.highTemp + "°F";
+        document.querySelector("#low-temp").textContent = "Today's Low: " + today.lowTemp + "°F";
+        document.querySelector("#chance-of-precipitation").textContent = "Chance of Precipitation: " + today.chanceOfPrecipitation + "%";
+        document.querySelector(".todays-weather-container").classList.remove("hidden");
+        document.querySelector(".forecast-header-container").classList.remove("hidden")
+        document.querySelector(".hottest-day-container").classList.remove("hidden")
+
+        highTempDay.textContent = "The Hottest day this week is " + highestTempDay;   
+
+        headerText.textContent = "Three Day Forecast";
+        let forecastList = document.querySelector(".three-day-weather-info");
+        forecastList.innerHTML = "";
+
+        for (let i = 0; i < threeDayForecast.length; i++) {
+            let forecastItem = document.createElement("div");
+            forecastItem.classList.add("forecast-box");
+        
+            let day = document.createElement("h3");
+            day.textContent = threeDayForecast[i].day;
+            forecastItem.appendChild(day);
+        
+            let temp = document.createElement("h1");
+            temp.textContent = `${threeDayForecast[i].averageTemp}°F`;
+            forecastItem.appendChild(temp);
+        
+            let highTemp = document.createElement("p");
+            highTemp.textContent = `High Temp: ${threeDayForecast[i].highTemp}°F`;
+            forecastItem.appendChild(highTemp);
+        
+            let lowTemp = document.createElement("p");
+            lowTemp.textContent = `Low Temp: ${threeDayForecast[i].lowTemp}°F`;
+            forecastItem.appendChild(lowTemp);
+        
+            let chanceOfPrecipitation = document.createElement("p");
+            chanceOfPrecipitation.textContent = `Chance of Precipitation: ${threeDayForecast[i].chanceOfPrecipitation}%`;
+            forecastItem.appendChild(chanceOfPrecipitation);
+        
+            forecastList.appendChild(forecastItem);
+
+
+        };
+        if (today.currentTemp > 80) {
+            return temperatureTypeDiv.textContent = "It's Hot Today!"
+        } else if (today.currentTemp < 40) {
+            return temperatureTypeDiv.textContent = "It's Cold Today!"
+        } else if (today.currentTemp > 100) {
+            return temperatureTypeDiv.textContent = "Excessive Heat Warning";
+        } else {
+            return temperatureTypeDiv.textContent = "The Weather is Average!"
+        };
+        
+        };
+
+        function getDayOfWeek(dateString) {
+            const date = new Date(`${dateString}T00:00:00Z`);
+            
+            const utcOffsetMinutes = date.getTimezoneOffset();
+            
+            const californiaTime = new Date(date.getTime() + utcOffsetMinutes * 60000);
+            
+            const options = { weekday: 'long' };
+            const formatter = new Intl.DateTimeFormat('en-US', options);
+            
+            return formatter.format(californiaTime);
         }
-        console.log(threeDayForecast, "Three Day Forecast");
-    })
-    //add stuff to screen
-    updateUI()
-})
-
-//show the data to the user
-function updateUI() {
-    cityElement.textContent = todayObj.currentCity;
-
-}
